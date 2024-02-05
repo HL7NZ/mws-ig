78dbc0453be1889c45ab4b3e7f31c4478e795a99
pryan <pat.ryan@health.govt.nz>
2024-02-05 03:13:33 +0000

    getting versions from sushi-config

--- openapi/makeoas.sh
+++ openapi/makeoas.sh
@@ -1,7 +1,10 @@
-# first step would be to get the openapi common oas file from hip-fhir-common mvn package
-# we are going to temporarily copy it to local openapi folder
+#!/bin/bash
+set -x #echo on
 
-cp openapi/HipFhirCommonOpenApi.yaml openapiHipFhirMwsOpenApi.yaml
+common_version=$( yq '.dependencies."hl7.org.nz.fhir.ig.hip-core".version' ./sushi-config.yaml)
+cp "./fhir_packages/hip-fhir-common-$common_version/openapi/HipFhirCommonOpenApi.yaml" ./openapi/HipFhirMwsOpenApi.yaml
+
+#add the MWS paths to the common openapi spec
 newpath=$(< ./openapi/HipFhirMwsPaths.yaml)  yq -i '.paths=env(newpath) ' openapi/HipFhirMwsOpenApi.yaml 
 newversion=$(yq '.version' ./sushi-config.yaml)   yq -i '.info.version=env(newversion) ' openapi/HipFhirMwsOpenApi.yaml 
 
